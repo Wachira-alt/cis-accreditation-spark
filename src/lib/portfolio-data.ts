51362d3592d93450cdf270de768524bfab2f4113
Dennis Wachira <wachiradenis336@gmail.com>
2026-07-19 21:32:41 +0300

    feat: update boarding handbook

--- src/lib/portfolio-data.ts
+++ src/lib/portfolio-data.ts
@@ -418,7 +418,7 @@ export const part2: Section = {
         },
         {
           label: "Boarding Policy",
-          href: "https://drive.google.com/file/d/1GdDU2Uh64xui2fV7zmydiU0ntuF_uvxi/view",
+          href: "https://drive.google.com/file/d/1mp21hM4URCQKuvQFjVYZAlQTowZ3YtOX/view?usp=drive_link",
         },
         {
           label: "Boarding Handbook",
